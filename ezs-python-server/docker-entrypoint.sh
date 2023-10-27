7baba8442ff0a0fa630c5a8828fb952a309868ca
François Parmentier <francois.parmentier@gmail.com>
2023-10-27 14:23:26 +0200

    refactor(ezs-python-server): Use USER statement
    And move COPY to the end of the Dockerfile.

--- ezs-python-server/docker-entrypoint.sh
+++ ezs-python-server/docker-entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-exec su - pn -c "cd /app && node generate-dotenv.js && cd /app/public && npx dotenv -e ../.env -- npx ezs --daemon ./"
+cd /app
+node generate-dotenv.js
+cd /app/public
+npx dotenv -e ../.env -- npx ezs --daemon ./
