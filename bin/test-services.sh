b503e1a7ffe5522f8884b2e518a36e7ddfe6997b
François Parmentier <francois.parmentier@gmail.com>
2023-12-14 08:54:26 +0100

    build: Adapt test-services.sh

--- bin/test-services.sh
+++ bin/test-services.sh
@@ -5,14 +5,11 @@
 set -u # No uninitialized variable
 set -o pipefail # Fail at first pipe error
 
-LOCATION=${1:-local}
-
-shift # Discard the first parameter
-SERVICES=("$@") # Get all parameters from the 2nd one to the last
+SERVICES=("$@") # Get all parameters
 
 for SERVICE in "${SERVICES[@]}"; do
     SERVICE=${SERVICE#services/} # Remove `services/`
-    npx hurl --test --continue-on-error "services/$SERVICE/$LOCATION-tests.hurl"
+    npx hurl --test --continue-on-error --variable host="https://$SERVICE.services.istex.fr" "services/$SERVICE/tests.hurl"
 done
 
 exit 0
