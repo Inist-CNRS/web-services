8042d19d9b64a8cdf86a433e7847bb6667f9df22
François Parmentier <francois.parmentier@gmail.com>
2024-01-30 11:07:37 +0100

    refactor(update-image): Wrap in a function
    So that it can call itself recursively

--- bin/update-images.sh
+++ bin/update-images.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
-# $1 should be in `ezs-python-server`, or `ezs-python-saxon-server` format
-# But `bases/ezs-python-server` is accepted too.
-CHANGING_BASE=${1/bases\//}
-CHANGING_BASE=${CHANGING_BASE%/}
+function updateImage() {
+    # $1 should be in `ezs-python-server`, or `ezs-python-saxon-server` format
+    # But `bases/ezs-python-server` is accepted too.
+    CHANGING_BASE=${1/bases\//}
+    CHANGING_BASE=${CHANGING_BASE%/}
 
-printf "Update images depending from %s\n\n" "$CHANGING_BASE"
+    printf "Update images depending from %s\n\n" "$CHANGING_BASE"
 
-BASE_IMAGE_VERSION=$(node -e "console.log(require('./bases/$CHANGING_BASE/package.json').version)")
-BASE_IMAGE_TAG_BEGINNING=$(node -e "console.log(require('./bases/$CHANGING_BASE/package.json').scripts.build.split(':')[1].split('{')[0].slice(0,-2))")
-BASE_IMAGE_TAG="$BASE_IMAGE_TAG_BEGINNING-$BASE_IMAGE_VERSION"
-printf "Tag of the image: %s\n\n" "$BASE_IMAGE_TAG"
+    BASE_IMAGE_VERSION=$(node -e "console.log(require('./bases/$CHANGING_BASE/package.json').version)")
+    BASE_IMAGE_TAG_BEGINNING=$(node -e "console.log(require('./bases/$CHANGING_BASE/package.json').scripts.build.split(':')[1].split('{')[0].slice(0,-2))")
+    BASE_IMAGE_TAG="$BASE_IMAGE_TAG_BEGINNING-$BASE_IMAGE_VERSION"
+    printf "Tag of the image: %s\n\n" "$BASE_IMAGE_TAG"
 
-DOCKERFILES=$(ls bases/*/Dockerfile template/Dockerfile services/*/Dockerfile)
+    DOCKERFILES=$(ls bases/*/Dockerfile template/Dockerfile services/*/Dockerfile)
 
 
-printf "Directly depending images:\n"
+    printf "Directly depending images:\n"
 
-DEPENDING_DOCKERFILES=$(grep -i "^FROM cnrsinist/$CHANGING_BASE:$BASE_IMAGE_TAG" $DOCKERFILES | sed -e 's/:.*$//')
+    DEPENDING_DOCKERFILES=$(grep -i "^FROM cnrsinist/$CHANGING_BASE:$BASE_IMAGE_TAG" $DOCKERFILES | sed -e 's/:.*$//')
 
-for DOCKERFILE in $DEPENDING_DOCKERFILES
-do
-    printf " - %s\n" "$DOCKERFILE"
-done
+    for DOCKERFILE in $DEPENDING_DOCKERFILES
+    do
+        printf " - %s\n" "$DOCKERFILE"
+    done
+
+}
+
+updateImage "$1"
