1b49a7a573df95f5a24350d64b38518f154cbca5
François Parmentier <francois.parmentier@gmail.com>
2024-01-30 13:29:22 +0100

    fix(update-image): Remove recursion
    As it does not work very well with global variables

--- bin/update-images.sh
+++ bin/update-images.sh
@@ -10,17 +10,11 @@ function dryRun() {
 #
 # @param {string} base image name in the format `ezs-python-server` or
 #                 `basees/ezs-python-saxon-server`
-# @param {number} [level=0] level of the recursion
 function updateBase() {
     # $1 should be in `ezs-python-server`, or `ezs-python-saxon-server` format
     # But `bases/ezs-python-server` is accepted too.
     CHANGING_BASE=${1/bases\//}
     CHANGING_BASE=${CHANGING_BASE%/}
-
-    LEVEL=${2:-0}
-    if [ "$LEVEL" -ge 1 ]; then
-        printf "\n"
-    fi
 
     printf "Update images depending from %s (level %s)\n\n" "$CHANGING_BASE" "$LEVEL"
 
@@ -46,14 +40,20 @@ function updateBase() {
     for DOCKERFILE in $DEPENDING_DOCKERFILES
     do
         printf " - %s\n" "$DOCKERFILE"
-        sed -i -e "s/cnrsinist\/$CHANGING_BASE:.*$/cnrsinist\/$CHANGING_BASE:$BASE_IMAGE_TAG/g" "$DOCKERFILE"
+        dryRun sed -i -e "s/cnrsinist\/$CHANGING_BASE:.*$/cnrsinist\/$CHANGING_BASE:$BASE_IMAGE_TAG/g" "$DOCKERFILE"
         IMAGE_DIR=$(dirname "$DOCKERFILE")
-        dryRun npm -w "$IMAGE_DIR" version patch
         TYPE=$(dirname "$IMAGE_DIR")
+        if [ "$IMAGE_DIR" = "template" ]; then
+            dryRun git add template
+            dryRun git commit -m "Update template"
+            dryRun git push
+        else
+            dryRun npm -w "$IMAGE_DIR" version patch
+        fi
         if [ "$TYPE" = "bases" ]; then
             dryRun npm -w "$IMAGE_DIR" run build
             dryRun npm -w "$IMAGE_DIR" run publish
-            updateBase "$IMAGE_DIR" $((LEVEL+1))
+            printf "\n***** Don't forget to run \"%s\" *******\n" "updateBase $IMAGE_DIR"
         fi
         printf "\n"
     done
