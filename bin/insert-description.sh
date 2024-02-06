4c1c2314fc2012e7bde4b5193946989ec9fc0f2a
François Parmentier <francois.parmentier@gmail.com>
2024-02-06 11:47:39 +0100

    fix(insert-description): Don't crash when file does not exist

--- bin/insert-description.sh
+++ bin/insert-description.sh
@@ -22,19 +22,23 @@ function insert_description() {
     MARKDOWN_PATH=$1
     SLASH_PATH=${MARKDOWN_PATH//_//}
     INI_PATH=${SLASH_PATH/%.md/.ini}
-    echo "$INI_PATH"
-    echo "-----------"
+    printf " - %s" "$INI_PATH"
+
+    if [ ! -f "$INI_PATH" ]; then
+        printf " X\n"
+        return
+    fi
 
     DESCRIPTION=$(markdown2line "$MARKDOWN_PATH")
 
     CONTAINS_DESCRIPTION=$(grep "post.description" "$INI_PATH")
-
     if [ "$CONTAINS_DESCRIPTION" = "" ]; then
         sed -i "/^post\.summary.*/a \
         post.description = $DESCRIPTION" "$INI_PATH"
     else
         sed -i "s/post.description =.*/post.description = $DESCRIPTION/" "$INI_PATH"
     fi
+    printf " ✓\n"
 }
 
 for file in "$@"; do
