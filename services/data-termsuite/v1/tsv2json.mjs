f985474102993f0b738ff19a841905c519df2203
François Parmentier <francois.parmentier@gmail.com>
2024-03-14 14:16:39 +0100

    fix(data-termsuite): tsv2json now prints line by line
    To avoid imbricating an array within an array

--- services/data-termsuite/v1/tsv2json.mjs
+++ services/data-termsuite/v1/tsv2json.mjs
@@ -5,6 +5,5 @@ const lines = tsv.split("\n");
 const value = lines
     .map((line) => line.split("\t"))
     .map((columns) => ({ key: columns[2], freq: Number(columns[3]) }))
-    .slice(1, -1); // remove header and empty line
-
-console.log(JSON.stringify(value))
+    .slice(1, -1) // remove header and empty line
+    .forEach(value => console.log(JSON.stringify(value)))
