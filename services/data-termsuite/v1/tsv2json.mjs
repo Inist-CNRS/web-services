15f12597d7ada623e5a8954b3f9bf9ec05e51196
François Parmentier <francois.parmentier@gmail.com>
2024-03-14 11:23:23 +0100

    feat(data-termsuite): Add /v1/en
    In the data-computer way.

---
+++ services/data-termsuite/v1/tsv2json.mjs
@@ -0,0 +1,10 @@
+import { readFileSync } from "fs";
+
+const tsv = readFileSync(0).toString("utf-8");
+const lines = tsv.split("\n");
+const value = lines
+    .map((line) => line.split("\t"))
+    .map((columns) => ({ key: columns[2], freq: Number(columns[3]) }))
+    .slice(1, -1); // remove header and empty line
+
+console.log(JSON.stringify(value))
