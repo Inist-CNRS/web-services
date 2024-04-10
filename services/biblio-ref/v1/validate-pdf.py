0437f361077737cb7a934ffe5b425ec4f17cdf27
Leo-gail <leo.gaillard987@gmail.com>
2024-04-10 09:37:44 +0200

    feat(biblio-ref): add pdf AND url route

---
+++ services/biblio-ref/v1/validate-pdf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import json
+from refextract import extract_references_from_file
+from bibref.bibref_functions import *
+
+
+
+for line in sys.stdin:
+    line0 = json.load(line)
+    pdf_filename = line0["filename"]
+    
+    try:
+        references_structured = extract_references_from_file(pdf_filename)
+    except:
+        references_structured = [""]
+        
+    try:
+        os.remove(pdf_filename)
+    except:
+        pass
+
+    references = []
+    for reference in references_structured:
+        if "author" in reference:
+            references.append(reference['raw_ref'][0])
+    
+    all_res = []
+    for reference in references:
+        res = biblio_ref(reference)
+        res['reference'] = reference
+        all_res.append(res)
+        
+    line0['value'] = all_res
+    sys.stdout.write(json.dumps(line0))
+    sys.stdout.write('\n')
---
+++ services/biblio-ref/v1/validate-url.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json
+import sys
+import random
+from bibref.bibref_functions import *
+import os
+from refextract import extract_references_from_file
+
+    
+for line in sys.stdin:
+    line0=json.loads(line)
+    # get URL of the PDF
+    url=line0['value']
+    name=str(round(random.random()*1000))+url.split('/')[-1]
+
+    # get PDF + convert it to txt
+    pdf_filename = '/tmp/'+name
+
+    try:
+        response = session_pdf.get(url)
+        response.raise_for_status()  # check if request succeeded
+
+        with open(pdf_filename, 'wb') as pdf_file:
+            pdf_file.write(response.content)
+
+        references_structured = extract_references_from_file(pdf_filename)
+        
+        os.remove(pdf_filename)
+    except:
+        references_structured = [""]
+
+    references = []
+    for reference in references_structured:
+        if "author" in reference:
+            references.append(reference['raw_ref'][0])
+    
+    all_res = []
+    for reference in references:
+        res = biblio_ref(reference)
+        res['reference'] = reference
+        res['url_pdf']=url
+        all_res.append(res)
+        
+    line0['value'] = all_res
+    sys.stdout.write(json.dumps(line0))
+    sys.stdout.write('\n')
