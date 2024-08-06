f12ff3cf4969f9c0c3a607d45cd428649c7b0941
leogail <leo.gaillard987@gmail.com>
2024-08-06 09:04:39 +0200

    feat(person-ner): create service

---
+++ services/person-ner/README.md
@@ -0,0 +1,5 @@
+# ws-person-ner@0.0.0
+
+Extraction d'entités nommées de noms de personnes
+
+Permet d'extraire les entités nommées correspondant à des noms de personnes dans du texte
---
+++ services/person-ner/v1/tagger.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import spacy
+import json
+import sys
+
+nlp = spacy.load("xx_ent_wiki_sm")
+
+for line in sys.stdin:
+    line = json.loads(line)
+    res = {"PER":[],"LOC":[],"ORG":[],"MISC":[]}
+    try:
+        value = line["value"]
+    except KeyError:
+        value = ""
+    
+    try:
+        doc = nlp(value)
+        doc = doc.ents
+    except Exception:
+        doc = []
+    
+    for ent in doc:
+        res[ent.label_].append(ent.text)
+        
+    line["value"] = res
+    
+    sys.stdout.write(json.dumps(line))
+    sys.stdout.write("\n")
