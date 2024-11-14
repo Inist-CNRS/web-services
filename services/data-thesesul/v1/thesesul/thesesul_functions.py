0b240e44cbf63cef3b03ddae8d10d1a50a1d7a5f
leogail <leo.gaillard987@gmail.com>
2024-11-14 11:40:36 +0100

    refactor(data-thesesul): add suggestion from code review (refactoring)

--- services/data-thesesul/v1/thesesul/thesesul_functions.py
+++ services/data-thesesul/v1/thesesul/thesesul_functions.py
@@ -122,3 +122,33 @@ def create_zip(sudoc_id):
     if os.path.exists(os.path.join(OUTPUT_DIR,f"{sudoc_id}.pdf")):
         os.remove(os.path.join(OUTPUT_DIR,f"{sudoc_id}.pdf"))
         os.remove(os.path.join(OUTPUT_DIR,f"{sudoc_id}.xml"))
+
+
+def get_hal_zip_from_sudoc_id(sudoc_id, log_file):
+    sudoc_file = get_xml_from_sudoc(sudoc_id)
+    if sudoc_file :
+        cond_file = transform_sudoc_to_conditor(sudoc_file)
+        if cond_file:
+            tei_hal_file = transform_conditor_to_tei_hal(cond_file)
+            if tei_hal_file:
+                if validate_tei_hal(tei_hal_file):
+                    input_file = os.path.join(TMP_DIR, f"tei_hal-{retrieve_id}.xml")
+                    res_file = os.path.join(OUTPUT_DIR, f'{sudoc_id}.xml')
+                    with open(input_file, 'r') as f_in:
+                        with open(res_file, 'w') as f_out:
+                            f_out.write(f_in.read())
+                            
+                    pdf_file = download_pdf(res_file)
+                    if not pdf_file:
+                        write_error_in_logs(log_file, sudoc_id, "Erreur : Récuperation du fichier PDF impossible")
+                    else:
+                        modify_target_in_xml(res_file, sudoc_id)
+                        create_zip(sudoc_id)
+                else:
+                    write_error_in_logs(log_file, sudoc_id, "Erreur : Fichier TEI généré invalide")
+            else:
+                write_error_in_logs(log_file, sudoc_id, "Erreur :  Schéma invalide (transformation XLST vers TEI-Hal impossible - 2)")
+        else:
+            write_error_in_logs(log_file, sudoc_id, "Erreur : Schéma invalide (transformation XLST vers TEI-Hal impossible - 1)")
+    else:
+        write_error_in_logs(log_file, sudoc_id, "Erreur : Récupération du fichier XML du Sudoc impossible")
