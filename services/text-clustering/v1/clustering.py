2276783e575e1977bff5261e1df047996f5939df
leogail <leo.gaillard987@gmail.com>
2024-12-03 11:42:52 +0100

    feat(text-clustering): change algorithm

--- services/text-clustering/v1/clustering.py
+++ services/text-clustering/v1/clustering.py
@@ -5,18 +5,17 @@ import sys
 import requests
 from sentence_transformers import SentenceTransformer
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics.pairwise import cosine_distances
-from sklearn.decomposition import PCA
-from sklearn.cluster import HDBSCAN
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+import umap
 
-# from prometheus_client import CollectorRegistry, Counter, push_to_gateway
-# registry = CollectorRegistry()
-# c = Counter('documents', 'Number of documents processed', registry=registry)
-# job_name='clustering'
-
-
-# Get the index of "p" param (given by the user) and assign it to "nb". 20 if not found
-nbTopic = sys.argv[sys.argv.index('-p') + 1] if '-p' in sys.argv else 20
+model = SentenceTransformer('./v1/all-MiniLM-L6-v2')
+n_keywords = 20
+nb_cluster = int(sys.argv[sys.argv.index('-p') + 1] if '-p' in sys.argv else 0)
+if nb_cluster > 30 :
+    nb_cluster = 30
+if nb_cluster < 2 and nb_cluster !=0 :
+    nb_cluster = 2
 
 
 def center_reduce(matrix):
@@ -37,6 +36,22 @@ def center_reduce(matrix):
     return matrix_center_reduce
 
 
+def find_optimal_k(distance_matrix, max_k):
+    silhouette_scores = []
+    
+    for k in range(2, max_k+1):
+        kmeans = KMeans(n_clusters=k, random_state=42)
+        kmeans.fit(distance_matrix)
+        
+        score = silhouette_score(distance_matrix, kmeans.labels_, metric='euclidean')
+        silhouette_scores.append(score)
+    
+    # keep the k who gives the best silouhette score
+    optimal_k = silhouette_scores.index(max(silhouette_scores)) + 2
+    
+    return optimal_k
+
+
 def teeft(data, n_keywords, language="en"):
     try:
         url = f"https://terms-extraction.services.istex.fr/v1/teeft/{language}?nb={n_keywords}"
@@ -51,8 +66,32 @@ def teeft(data, n_keywords, language="en"):
         return []
 
 
-model = SentenceTransformer('./v1/all-MiniLM-L6-v2')
-n_keywords = 20
+def filter_keywords(data, threshold):
+    """function who filter teeft results
+
+    Args:
+        keywords_dict (dict): dict to filter
+        threshold (float): thereshold
+
+    Returns:
+        dict: dict filtered
+    """
+    word_count = {}
+    
+    for key, keywords in data.items():
+        for keyword in keywords:
+            if keyword in word_count:
+                word_count[keyword] += 1
+            else:
+                word_count[keyword] = 1
+    
+    filtered_data = {}
+    
+    for key, keywords in data.items():
+        filtered_keywords = [keyword for keyword in keywords if word_count[keyword]/len(data) <= threshold]
+        filtered_data[key] = filtered_keywords
+    
+    return filtered_data
 
 
 ## WS
@@ -90,23 +129,15 @@ for i in range(len_data):
 
 
 # Dimension reduction
-pca = PCA(n_components=0.95)
-embeddings = pca.fit_transform(center_reduce(texts))
-cosine_dist_matrix = cosine_distances(embeddings, embeddings)
+umap_model = umap.UMAP(n_neighbors=max(10, min(30,int(len_data/20))), n_components=10, metric='cosine', random_state=42, min_dist=0.0)
+reduced_embeddings = umap_model.fit_transform(texts)
 
+if nb_cluster == 0:
+    nb_cluster = find_optimal_k(reduced_embeddings, max_k=30)
 
-# HDBSCAN with scikit-learn
-clusterer = HDBSCAN(
-    algorithm='auto',
-    metric='precomputed',
-    min_cluster_size=int(max(5,len_data/20)),
-    cluster_selection_epsilon = 0.05,
-    min_samples=2,
-    cluster_selection_method="eom",
-    n_jobs=-1) 
-
-
-clusterer.fit(cosine_dist_matrix)
+# Clustering
+clusterer = KMeans(n_clusters=nb_cluster, random_state=42)
+clusterer.fit(reduced_embeddings)
 
 
 # Create datas for teeft
@@ -127,20 +158,24 @@ n_clusters = len(keywords)
 for i in range(n_clusters):
     data = {"id": i+1, "value": keywords[i+1]}
     keywords[i+1] = teeft(data, n_keywords)
+
+# Filter dict : delete every keywords who has a to big frequency
+try:
+    keywords = filter_keywords(keywords, threshold=0.5)
+except:
+    pass
 # Add res for noise cluster
 keywords[0] = []
-
 
 # extract infos and return teeft res
 indice_in_cluster=0
 for i in range(len_data):
     if i in indice_out_cluster :
-        all_data[i]["value"] = {"cluster":0, "weight":"1.0", "keywords": []}
+        all_data[i]["value"] = {"cluster":0, "keywords": []}
     else:
         label = int(clusterer.labels_[indice_in_cluster]+1)
         all_data[i]["value"]={
             "cluster":label,
-            "weight":str(clusterer.probabilities_[indice_in_cluster]),
             "keywords":keywords[label]
             }
         indice_in_cluster +=1 # Here we increment only if the row isn't noise, because they aren't count in "clusterer model"
