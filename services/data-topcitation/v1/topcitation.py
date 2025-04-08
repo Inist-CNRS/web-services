f76b973dc0deaf494c0ef7d5408d2008c49e057d
revol <justine.revol@inist.fr>
2025-04-08 14:19:26 +0200

    fix(data-topcitation): corrects referenced_works empty field error

--- services/data-topcitation/v1/topcitation.py
+++ services/data-topcitation/v1/topcitation.py
@@ -19,10 +19,13 @@ def extract_doi_referenced_works(data):
         doi_url = data.get('doi')
         referenced_works = data.get('referenced_works', [])
         
-        return {doi_url: referenced_works}
+        if not referenced_works:
+            return {doi_url: {"referenced_works": "champs referenced_works vide"}}
+        
+        return {doi_url: {"referenced_works": referenced_works}}
     else:
         return {}
-    
+
 def openAlex_to_doi(url) :
 
     url_parse = url.split("/")
@@ -64,14 +67,19 @@ def main():
 
     # parcours le dictionnaire où les dois sont des clés et les références des listes de valeurs
     for doi, references in all_references.items():
+        if references["referenced_works"] == "champs referenced_works vide":
+            # Ajouter une entrée dans le JSON indiquant que le champ referenced_works est vide
+            sys.stdout.write(json.dumps({"id": doi, "value": {"message": "champs referenced_works vide"}}))
+            sys.stdout.write("\n")
+        else : 
         # itération de la liste des références
-        for citation in references:
-            # chaque citation est un clé qui a comme valeur les clés count et doi
-            # count est une clé qui a pour valeur le nombre de fois qu'apparait une citation
-            citation_count[citation]["count"] += 1
-            # doi est une clé qui a pour valeur le ou les doi(s) qui cite la citation
-            citation_count[citation]["doi"].append(doi)
-# exemple : {'https://openalex.org/W107566402': {'count': 1, 'doi': ['https://doi.org/10.1007/s10844-014-0317-4']}
+            for citation in references:
+                # chaque citation est un clé qui a comme valeur les clés count et doi
+                # count est une clé qui a pour valeur le nombre de fois qu'apparait une citation
+                citation_count[citation]["count"] += 1
+                # doi est une clé qui a pour valeur le ou les doi(s) qui cite la citation
+                citation_count[citation]["doi"].append(doi)
+    # exemple : {'https://openalex.org/W107566402': {'count': 1, 'doi': ['https://doi.org/10.1007/s10844-014-0317-4']}
 
     # utilisation de la méthode sorted() pour trier le dictionnaire en fonciton de count
     sorted_citations = sorted(citation_count.items(), key=lambda item: item[1]['count'], reverse=True)
