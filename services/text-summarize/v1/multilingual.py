999f3521d8c9b0c6ca24dc7bb3ca3f9e298236c8
leogail <leo.gaillard987@gmail.com>
2025-04-18 08:55:11 +0200

    feat(text-summarize):update model

--- services/text-summarize/v1/multilingual.py
+++ services/text-summarize/v1/multilingual.py
@@ -9,12 +9,12 @@ model = AutoModelForSeq2SeqLM.from_pretrained("./v1/mbart-large-50-finetuned-sum
 
 
 # Fonction pour générer un résumé à partir d'un texte
-def generate_summary(text, minimum_size, maximum_size):
+def generate_summary(text):
     input_ids = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=1024).input_ids
     if input_ids.shape[1] < 250:
         return text
 
-    outputs = model.generate(input_ids, min_new_tokens=minimum_size, max_new_tokens=maximum_size)
+    outputs = model.generate(input_ids)
     summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return summary
 
@@ -24,7 +24,7 @@ for line in sys.stdin:
     if "value" in data:
         text = data["value"]
         if isinstance(data["value"], str):
-            data["value"] = generate_summary(text, 80, 250)
+            data["value"] = generate_summary(text) + " <AI-generated>"
         else:
             data["value"] = ""
     else:
