25216989d13f7d42c54f2df62968c239ea356971
François Parmentier <francois.parmentier@gmail.com>
2025-08-25 14:19:46 +0200

    fix(address-kit): when a CNRS unity code is found, say it's France
    And lint.

--- services/address-kit/v1/affiliationcountry/detect_country.py
+++ services/address-kit/v1/affiliationcountry/detect_country.py
@@ -13,12 +13,16 @@ import re
 import json
 import sys
 
-osm = Nominatim(timeout=60,user_agent='inistTDM')
+osm = Nominatim(timeout=60, user_agent="inistTDM")
 
-locator = functools.lru_cache(maxsize=128)(functools.partial(osm.geocode, timeout=60,language='en'))
-locator_rev = functools.lru_cache(maxsize=128)(functools.partial(osm.reverse, timeout=60,language='en'))
+locator = functools.lru_cache(maxsize=128)(
+    functools.partial(osm.geocode, timeout=60, language="en")
+)
+locator_rev = functools.lru_cache(maxsize=128)(
+    functools.partial(osm.reverse, timeout=60, language="en")
+)
 
-#fin=json.loads(json.dumps([{
+# fin=json.loads(json.dumps([{
 #        "id":"1",
 #        "value":"université sciences et technologies bordeaux 1 institut national de physique nucléaire et de physique des particules du cnrs in2p3 UMR5797"},
 #    {"id":2,
@@ -49,212 +53,269 @@ locator_rev = functools.lru_cache(maxsize=128)(functools.partial(osm.reverse, ti
 #      "value":"Researcher with grant Bocconi University ."},
 #     {"id":14,
 #      "value":" University of Toulouse, INPT, INP‑PURPAN, 75 voie du T.O.E.C., FR‑31076 TOULOUSE. Email: regis.vezian@purpan.fr"}
-#      
+#
 #        ]))
 
 
+def place_address(address):
+    place, (lat, lng) = locator(address)
+    res = locator_rev(str(lat) + "," + str(lng))
+    try:
+        country = res.raw["address"]["country"]
+    except KeyError:
+        country = res.raw["address"]["country_code"]
+    try:
+        code = res.raw["address"]["country_code"]
+    except KeyError:
+        code = ""
 
-def placeaddress(address):
-    place, (lat, lng)=locator(address)
-    res=locator_rev(str(lat)+","+str(lng))
-    try :
-        country=res.raw['address']['country']
-    except KeyError:
-        country=res.raw['address']['country_code']
     try:
-        code=res.raw['address']['country_code']
+        village = res.raw["address"]["village"]
     except KeyError:
-            code=""
-        
-    try:
-        village=res.raw['address']['village']
-    except KeyError:
-        village=''
-    return country,lat,lng,village,code
+        village = ""
+    return country, lat, lng, village, code
 
 
 for line in sys.stdin:
-    country=""
-    code=""
-    a2=""
-    a3=""
-    data=json.loads(line)
+    country = ""
+    code = ""
+    a2 = ""
+    a3 = ""
+    data = json.loads(line)
 
-    aff=data['value']
-    lines=aff.replace('"','')
-    adr=lines.strip('.')
-    adr=adr.strip()
-    
-    adr=adr.replace('&amp;',';')
-    ad=adr.split(';')
-    ads=[i.strip() for i in ad]
+    aff = data["value"]
+    lines = aff.replace('"', "")
+    adr = lines.strip(".")
+    adr = adr.strip()
 
-    for a in set(ads) :
+    adr = adr.replace("&amp;", ";")
+    ad = adr.split(";")
+    ads = [i.strip() for i in ad]
 
-        a=a.lower()
-        a=a.replace('cedex','')
-        a=re.sub(' U[0-9]+','',a,flags=re.IGNORECASE)
-        a=re.sub("both in ",'',a,flags=re.IGNORECASE)
-        a=re.sub("researcher with grant",'',a,flags=re.IGNORECASE)
-        a=re.sub(' UMR[0-9]+','',a,flags=re.IGNORECASE)
-        a=a.replace('...',' ')
-        a=a.replace('_',' ')
-        a=re.sub('\([a-zA-Z0-9 ,:\–]+\)','',a)
-      
-        ars=re.split(',|:',a)
-        if re.match(r'u[amsrp]+ ?[0-9]+',a):
-            country='France'
-            code='fr'
+    for a in set(ads):
+
+        a = a.lower()
+        a = a.replace("cedex", "")
+        a = re.sub(" U[0-9]+", "", a, flags=re.IGNORECASE)
+        a = re.sub("both in ", "", a, flags=re.IGNORECASE)
+        a = re.sub("researcher with grant", "", a, flags=re.IGNORECASE)
+        # a = re.sub(" UMR[0-9]+", "", a, flags=re.IGNORECASE)
+        a = a.replace("...", " ")
+        a = a.replace("_", " ")
+        a = re.sub(r"\([a-zA-Z0-9 ,:\–]+\)", "", a)
+
+        ars = re.split(",|:", a)
+        if re.search(r"u[amsrp]+ ?[0-9]+", a):
+            country = "France"
+            code = "fr"
         else:
             try:
 
-                country=placeaddress(country)[0]
-                code=placeaddress(country)[4]
-    
-    
-            except (KeyError,TypeError) as error:
-                #- 2 s'il n'y a pas de pays avec libpostal on essaye avec nominatim de trouver un pays
-    
+                country = place_address(country)[0]
+                code = place_address(country)[4]
+
+            except (KeyError, TypeError):
+                # - 2 s'il n'y a pas de pays avec libpostal on essaye avec nominatim de trouver un pays
+
                 try:
-    
-                    country=placeaddress(a)[0]
-                    code=placeaddress(a)[4]
-    
-                    if placeaddress(a)[3].lower()!=a.lower():
-    
-    
-                        country=country
-    
-                #-3 si ça ne fonctionne pas on essaye de spliter l'adresse et de traiter les éléments séparéément
-                except (KeyError,TypeError) as error:
-    
-                    a=a.lower()
-                    a2=''
-    
-                    if "university" in unidecode.unidecode(a).lower():#.split()[:-1]:
-    
-                        a1=re.findall("faculty of [a-zA-Z]+",unidecode.unidecode(a), flags=re.IGNORECASE)
-                        a11=re.findall("college of [a-zA-Z]+",unidecode.unidecode(a), flags=re.IGNORECASE)
-                        if len(a1)>0:
-                            a=a.replace(a1[0],'')           
-                        if len(a11)>0:
-                            a=a.replace(a11[0],'')
+
+                    country = place_address(a)[0]
+                    code = place_address(a)[4]
+
+                    if place_address(a)[3].lower() != a.lower():
+
+                        country = country
+
+                # -3 si ça ne fonctionne pas on essaye de spliter l'adresse et de traiter les éléments séparément
+                except (KeyError, TypeError):
+
+                    a = a.lower()
+                    a2 = ""
+
+                    if "university" in unidecode.unidecode(a).lower():
+
+                        a1 = re.findall(
+                            "faculty of [a-zA-Z]+",
+                            unidecode.unidecode(a),
+                            flags=re.IGNORECASE,
+                        )
+                        a11 = re.findall(
+                            "college of [a-zA-Z]+",
+                            unidecode.unidecode(a),
+                            flags=re.IGNORECASE,
+                        )
+                        if len(a1) > 0:
+                            a = a.replace(a1[0], "")
+                        if len(a11) > 0:
+                            a = a.replace(a11[0], "")
                         if "university" in a.lower().split()[:-1]:
-                    
-                            a=a.replace(' hospital ',' ')
-    
-                            a2=re.findall("university [ 2a-zA-Z&]+",unidecode.unidecode(a), flags=re.IGNORECASE)
-                            if len(a2)>=1:
-                          
-                                a3=a2[0].replace("university of ","university ")
-                            if len(a2)<1:
-                                a2=unidecode.unidecode(a).lower().replace('university','$ university').split('$ ')
-                            a2.insert(0,a3)
+
+                            a = a.replace(" hospital ", " ")
+
+                            a2 = re.findall(
+                                "university [ 2a-zA-Z&]+",
+                                unidecode.unidecode(a),
+                                flags=re.IGNORECASE,
+                            )
+                            if len(a2) >= 1:
+
+                                a3 = a2[0].replace("university of ", "university ")
+                            if len(a2) < 1:
+                                a2 = (
+                                    unidecode.unidecode(a)
+                                    .lower()
+                                    .replace("university", "$ university")
+                                    .split("$ ")
+                                )
+                            a2.insert(0, a3)
                             try:
-                                a2.remove('university of science')
+                                a2.remove("university of science")
                             except ValueError:
-                                uos=0
-                        elif ("university" ==a.split()[-1].lower()) or ("university," in a.lower()):
-                            a2=re.findall("[ a-zA-Z]+ university",unidecode.unidecode(a), flags=re.IGNORECASE)
-                            a2.insert(0,'')
-    
+                                uos = 0
+                        elif ("university" == a.split()[-1].lower()) or (
+                            "university," in a.lower()
+                        ):
+                            a2 = re.findall(
+                                "[ a-zA-Z]+ university",
+                                unidecode.unidecode(a),
+                                flags=re.IGNORECASE,
+                            )
+                            a2.insert(0, "")
+
                     elif "universite " in unidecode.unidecode(a).lower():
-    
-                        a2=re.findall("universite (?:[A-Za-z]+\s){1}[A-Za-z]+",unidecode.unidecode(a), flags=re.IGNORECASE)
-                        a3=re.findall("(?:[A-Za-z]+\s){1}[A-Za-z]+ universite",unidecode.unidecode(a), flags=re.IGNORECASE)
-                        if len(a3)==0:
-                            a3=[' ']
-                        a2.insert(0,a3[0])
-                        if len(a2)<2:
-                            a2=unidecode.unidecode(a).lower().replace('universite','$ universite').split('$ ')
-    
+
+                        a2 = re.findall(
+                            "universite (?:[A-Za-z]+\s){1}[A-Za-z]+",
+                            unidecode.unidecode(a),
+                            flags=re.IGNORECASE,
+                        )
+                        a3 = re.findall(
+                            "(?:[A-Za-z]+\s){1}[A-Za-z]+ universite",
+                            unidecode.unidecode(a),
+                            flags=re.IGNORECASE,
+                        )
+                        if len(a3) == 0:
+                            a3 = [" "]
+                        a2.insert(0, a3[0])
+                        if len(a2) < 2:
+                            a2 = (
+                                unidecode.unidecode(a)
+                                .lower()
+                                .replace("universite", "$ universite")
+                                .split("$ ")
+                            )
+
                     elif "universidad " in unidecode.unidecode(a).lower():
-                        a2=re.findall("universidad [ a-zA-Z]+",unidecode.unidecode(a), flags=re.IGNORECASE)
-                        a2.insert(0,'')
-                        if len(a2)<2:
-                            a2=unidecode.unidecode(a).lower().replace('universidad','$ universidad').split('$ ')
-       
+                        a2 = re.findall(
+                            "universidad [ a-zA-Z]+",
+                            unidecode.unidecode(a),
+                            flags=re.IGNORECASE,
+                        )
+                        a2.insert(0, "")
+                        if len(a2) < 2:
+                            a2 = (
+                                unidecode.unidecode(a)
+                                .lower()
+                                .replace("universidad", "$ universidad")
+                                .split("$ ")
+                            )
+
                     elif "universitat " in unidecode.unidecode(a).lower():
-                        a2=re.findall("universitat [ a-zA-Z]+",unidecode.unidecode(a), flags=re.IGNORECASE)
-                        a2.insert(0,'')
-                        if len(a2)<2:
-                            a2=unidecode.unidecode(a).lower().replace('universitat','$ universitat').split('$ ')
-    
+                        a2 = re.findall(
+                            "universitat [ a-zA-Z]+",
+                            unidecode.unidecode(a),
+                            flags=re.IGNORECASE,
+                        )
+                        a2.insert(0, "")
+                        if len(a2) < 2:
+                            a2 = (
+                                unidecode.unidecode(a)
+                                .lower()
+                                .replace("universitat", "$ universitat")
+                                .split("$ ")
+                            )
+
                     elif "hopital " in unidecode.unidecode(a).lower():
-                        a2=re.findall("hopital [a-zA-Z']+",unidecode.unidecode(a), flags=re.IGNORECASE)
-                        a2.insert(0,'')
-    
+                        a2 = re.findall(
+                            "hopital [a-zA-Z']+",
+                            unidecode.unidecode(a),
+                            flags=re.IGNORECASE,
+                        )
+                        a2.insert(0, "")
+
                     elif ("chu" and "chru") in unidecode.unidecode(a).lower():
                         if ("chu de " and "chru de ") in unidecode.unidecode(a).lower():
-                            a2=re.findall("ch[r]?u de [a-zA-Z']+",unidecode.unidecode(a), flags=re.IGNORECASE)
-                            a2.insert(0,'')
+                            a2 = re.findall(
+                                "ch[r]?u de [a-zA-Z']+",
+                                unidecode.unidecode(a),
+                                flags=re.IGNORECASE,
+                            )
+                            a2.insert(0, "")
                         else:
-                            
-                            a2=re.findall("ch[r]?u [a-zA-Z']+",unidecode.unidecode(a), flags=re.IGNORECASE)
-                            a3=a2[0].replace("chru","chu")
-                            a2.insert(0,a3)
-                   
-                    elif ',' in a: 
-                        a2=a.split(',')
-    
-                    elif ':' in a:
-                        a2=a.split(':')
-    
+
+                            a2 = re.findall(
+                                "ch[r]?u [a-zA-Z']+",
+                                unidecode.unidecode(a),
+                                flags=re.IGNORECASE,
+                            )
+                            a3 = a2[0].replace("chru", "chu")
+                            a2.insert(0, a3)
+
+                    elif "," in a:
+                        a2 = a.split(",")
+
+                    elif ":" in a:
+                        a2 = a.split(":")
+
                     elif " et " in a.split()[:-2]:
-    
-                        a2=a.split(' et ')
-    
-                    elif (' — ' and ' - ') in a:
-    
-                        if ' — ' in a:
-                            a2=a.split(' — ')
+
+                        a2 = a.split(" et ")
+
+                    elif (" — " and " - ") in a:
+
+                        if " — " in a:
+                            a2 = a.split(" — ")
                         else:
-                            a2=a.split(' - ')
-                    elif ' an der ' in a:
-    
-                        a2=a.split(' an der ')
-                    elif '-' in a:
-    
-                        a2=re.findall(r'\w+(?:-\w+)+',a)
-                        a2.insert(0,'')
-    
-                    if len(a2)>1:
-                        lon=1
+                            a2 = a.split(" - ")
+                    elif " an der " in a:
+
+                        a2 = a.split(" an der ")
+                    elif "-" in a:
+
+                        a2 = re.findall(r"\w+(?:-\w+)+", a)
+                        a2.insert(0, "")
+
+                    if len(a2) > 1:
+                        lon = 1
                         for aa in reversed(a2):
-                            aa=aa.strip()
-                            #time.sleep(1)
+                            aa = aa.strip()
+                            # time.sleep(1)
                             try:
-    
-                                country=placeaddress(aa)[0]
-                                code=placeaddress(aa)[4]
-                                lat=placeaddress(aa)[1]
-                                lng=placeaddress(aa)[2]
-    
+
+                                country = place_address(aa)[0]
+                                code = place_address(aa)[4]
+                                lat = place_address(aa)[1]
+                                lng = place_address(aa)[2]
+
                                 break
                             except TypeError:
-                                to=1
-    
-                                if lon==len(a2):
-                                    country=""#"unknown"
-    
-                            lon+=1
+                                to = 1
 
-                    else: 
-                        country=""#"Unknown"
+                                if lon == len(a2):
+                                    country = ""  # "unknown"
 
-    aa=''
+                            lon += 1
+
+                    else:
+                        country = ""  # "Unknown"
+
+    aa = ""
 
     try:
 
-        data["value"]={'country':country, 'code':code}
+        data["value"] = {"country": country, "code": code}
     except KeyError:
 
-        data["value"]={'country':country, 'code':""}
+        data["value"] = {"country": country, "code": ""}
     sys.stdout.write(json.dumps(data))
-    sys.stdout.write('\n')
-
-                    
-            
-        
-
-
+    sys.stdout.write("\n")
