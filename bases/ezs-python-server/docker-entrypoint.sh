6258ba45a6ce706538f1d4872b42b8967c55fe44
François Parmentier <francois.parmentier@gmail.com>
2025-09-05 15:29:44 +0200

    build(ezs-python-server): Update npm packages

--- bases/ezs-python-server/docker-entrypoint.sh
+++ bases/ezs-python-server/docker-entrypoint.sh
@@ -8,5 +8,12 @@ cd /app || exit 1
 node generate-dotenv.js
 cd /app/public || exit 2
 
+# Temporary fix for ezs bin
+# Likely due to old npm and node versions
+# TODO: remove when node version updated to 24
+mv /app/node_modules/@ezs/core/bin/ezs /app/node_modules/@ezs/core/bin/ezs.js
+rm /app/node_modules/.bin/ezs
+ln -s /app/node_modules/@ezs/core/bin/ezs.js /app/node_modules/.bin/ezs
+
 # Run ezs server as daemon user
-su -s /bin/bash daemon -c "npx dotenv -e ../.env -- npx ezs --daemon ./"
+su -s /bin/bash daemon -c "npx dotenv -e ../.env -- /app/node_modules/@ezs/core/bin/ezs.js --daemon ./"
