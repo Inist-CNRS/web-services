784f02b2aae3f3f7512ae7e369b100bae28ebac1
François Parmentier <francois.parmentier@gmail.com>
2026-02-09 14:55:31 +0100

    fix(data-graph): Retrieve the PID

--- services/data-graph/v1/graph.py
+++ services/data-graph/v1/graph.py
@@ -5,8 +5,8 @@ import sys
 from ressources import *
 import time
 
-thresh_edge = sys.argv[sys.argv.index('-p') + 1] if '-p' in sys.argv else "auto"
-thresh_node = sys.argv[sys.argv.index('-q') + 1] if '-q' in sys.argv else "auto"
+thresh_edge = sys.argv[sys.argv.index("-p") + 1] if "-p" in sys.argv else "auto"
+thresh_node = sys.argv[sys.argv.index("-q") + 1] if "-q" in sys.argv else "auto"
 
 print(thresh_edge, thresh_node, file=sys.stderr)
 
@@ -14,11 +14,12 @@ print(thresh_edge, thresh_node, file=sys.stderr)
 lines = []
 for line in sys.stdin:
     data = json.loads(line)
-    if not len(lines):
-        pid = data[[x for x in list(data.keys()) if x.startswith("PID")][0]][5:]
+    isPid = [x for x in list(data.keys()) if x.startswith("PID")]
+    if len(isPid) > 0:
+        pid = data[isPid[0]][5:]
     lines.append(data["value"])
 
-print("PID ",pid, file=sys.stderr)
+print("PID ", pid, file=sys.stderr)
 print(time.strftime("%A %d %B %Y %H:%M:%S"), file=sys.stderr)
 keyword = []
 L = []
@@ -31,10 +32,10 @@ for line in lines:
         else:
             freq[w] += 1
 
-freq = {k: v for k, v in sorted(freq.items(), key=lambda item: item[1],reverse=True)}
-#print(freq, file=sys.stderr)
+freq = {k: v for k, v in sorted(freq.items(), key=lambda item: item[1], reverse=True)}
+# print(freq, file=sys.stderr)
 
-#Seuil node
+# Seuil node
 if thresh_node == "auto":
     if len(freq) < 100:
         thresh_node = 1
@@ -54,7 +55,13 @@ if thresh_node == "auto":
                 if cumulative >= target:
                     thresh_node = freq[key]
                     break
-        print("Selected treshold : ",threshold, "differents words : ", diff_words, file=sys.stderr)
+        print(
+            "Selected threshold : ",
+            threshold,
+            "different words : ",
+            diff_words,
+            file=sys.stderr,
+        )
 else:
     thresh_node = int(thresh_node)
 
@@ -66,28 +73,28 @@ for liste in L:
     if len(l) > 0:
         keyword.append(l)
 
-#Seuil edge
+# Seuil edge
 if thresh_edge == "auto":
     if thresh_node == 1:
         thresh_edge = 0
     else:
-        thresh_edge = thresh_node/6
-        if thresh_edge < 1 :
+        thresh_edge = thresh_node / 6
+        if thresh_edge < 1:
             thresh_edge = 1
 else:
     thresh_edge = int(thresh_edge)
 
 
 print(thresh_node, thresh_edge, file=sys.stderr)
-#print(keyword, file=sys.stderr)
-node_weight,edge_weight,ignore_edge = get_weights(keyword,thresh_edge)
-#print(node_weight, file=sys.stderr)
-G = build_graph(node_weight,edge_weight)
-partition,communities = build_partition(G)
+# print(keyword, file=sys.stderr)
+node_weight, edge_weight, ignore_edge = get_weights(keyword, thresh_edge)
+# print(node_weight, file=sys.stderr)
+G = build_graph(node_weight, edge_weight)
+partition, communities = build_partition(G)
 
 linefeed = "\n"
-gexf = linefeed.join(plot_2D(G,partition,ignore_edge, pid))
+gexf = linefeed.join(plot_2D(G, partition, ignore_edge, pid))
 
 print("Finished", file=sys.stderr)
-sys.stdout.write(json.dumps({"value":gexf}))
-sys.stdout.write('\n')
+sys.stdout.write(json.dumps({"value": gexf}))
+sys.stdout.write("\n")
