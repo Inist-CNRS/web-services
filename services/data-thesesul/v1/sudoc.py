e730dc327fb91f16572a25080109fb6ca5132547
leogail <leo.gaillard987@gmail.com>
2026-03-04 15:13:37 +0100

    refactor(data-thesesul): upgrade code lisibility

--- services/data-thesesul/v1/sudoc.py
+++ services/data-thesesul/v1/sudoc.py
@@ -1,38 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-from thesesul.thesesul_functions import *
+import json
+import os
+import sys
+import subprocess
+import thesesul.thesesul_functions as th_fu
 
 if __name__ == "__main__":
-    log_file = str(os.path.join(TMP_DIR, "retrieve", retrieve_id, "log.csv"))
+    TMPDIR = th_fu.TMP_DIR
+    retrieve_id = th_fu.retrieve_id
+
+    log_file = str(os.path.join(TMPDIR, "retrieve", retrieve_id, "log.csv"))
     with open(log_file, 'w') as f_log:
-        f_log.write(f"ID du fichier,Erreur"+ '\n')
+        f_log.write("ID du fichier,Erreur" + '\n')
 
     for line in sys.stdin:
         try:
             line = json.loads(line)
             sudoc_id = line["value"]
-            
+
             if sudoc_id == "":
                 continue
 
-        except:
-            write_error_in_logs(log_file, "n/a", "Erreur : la liste d'identifiants Sudoc comporte des erreurs")
+        except Exception:
+            th_fu.write_error_in_logs(log_file, "n/a", "Erreur : la liste d'identifiants Sudoc comporte des erreurs")
             continue
-        
+
         try:
-            get_hal_zip_from_sudoc_id(sudoc_id, log_file)
+            th_fu.get_hal_zip_from_sudoc_id(sudoc_id, log_file)
         finally:
             # Suppression des fichiers temporaires
-            delete_files()
+            th_fu.delete_files()
     cmd = (
         f'cd /tmp/retrieve/{retrieve_id} && '
         f'tar -czf /tmp/retrieve/{retrieve_id}.tar.gz . && '
         f'cd /app/public/ && '
         f'rm -r /tmp/retrieve/{retrieve_id} && '
         f'mv /tmp/retrieve/{retrieve_id}.tar.gz /tmp/retrieve/{retrieve_id}'
-    )    
+    )
     result = subprocess.run(cmd, shell=True, capture_output=False)
 
-    sys.stdout.write(json.dumps({"value":"empty, just to proc webhook"}))
+    sys.stdout.write(json.dumps({"value": "empty, just to proc webhook"}))
     sys.stdout.write("\n")
